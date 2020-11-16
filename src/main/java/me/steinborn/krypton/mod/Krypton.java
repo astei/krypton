fc8222ea12eb4b1172a08b4a739c5b0781b5f645
Andrew Steinborn <git@steinborn.me>
2020-11-16 04:07:40 -0500

    Add some emoji!

--- src/main/java/me/steinborn/krypton/mod/Krypton.java
+++ src/main/java/me/steinborn/krypton/mod/Krypton.java
@@ -1,10 +1,14 @@
 package me.steinborn.krypton.mod;
 
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Krypton implements ModInitializer {
+    private static final Logger LOGGER = LogManager.getLogger(Krypton.class);
+
     @Override
     public void onInitialize() {
-
+        LOGGER.info("Krypton is now accelerating your Minecraft server's networking stack \uD83D\uDE80");
     }
 }
--- src/main/java/me/steinborn/krypton/mod/client/KryptonClient.java
+++ src/main/java/me/steinborn/krypton/mod/client/KryptonClient.java
@@ -1,13 +1,19 @@
 package me.steinborn.krypton.mod.client;
 
+import me.steinborn.krypton.mod.Krypton;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Environment(EnvType.CLIENT)
 public class KryptonClient implements ClientModInitializer {
+    private static final Logger LOGGER = LogManager.getLogger(Krypton.class);
+
     @Override
     public void onInitializeClient() {
-
+        LOGGER.info("Krypton is now accelerating your Minecraft client's networking stack \uD83D\uDE80");
+        LOGGER.info("Note that Krypton is most effective on servers, not the client.");
     }
 }

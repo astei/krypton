3289147ec7d2db70a5edd32a068af52e5e1472ad
Andrew Steinborn <git@steinborn.me>
2020-11-22 04:11:09 -0500

    Correctly switch to server entrypoint, fixes #4

--- src/main/java/me/steinborn/krypton/mod/Krypton.java
+++ src/main/java/me/steinborn/krypton/mod/Krypton.java
@@ -1,14 +1,14 @@
 package me.steinborn.krypton.mod;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.DedicatedServerModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Krypton implements ModInitializer {
+public class Krypton implements DedicatedServerModInitializer {
     private static final Logger LOGGER = LogManager.getLogger(Krypton.class);
 
     @Override
-    public void onInitialize() {
+    public void onInitializeServer() {
         LOGGER.info("Krypton is now accelerating your Minecraft server's networking stack \uD83D\uDE80");
     }
 }

5653a91b0cc86cbdf46688bb42a2d33689118ce1
Andrew Steinborn <git@steinborn.me>
2020-12-16 14:37:03 -0500

    Actually fix Fabric API incompatibility
    Fixes #6

--- src/main/java/me/steinborn/krypton/mixin/network/flushconsolidation/ClientConnectionMixin.java
+++ src/main/java/me/steinborn/krypton/mixin/network/flushconsolidation/ClientConnectionMixin.java
@@ -45,7 +45,7 @@ public abstract class ClientConnectionMixin implements ConfigurableAutoFlush {
     @Inject(locals = LocalCapture.CAPTURE_FAILHARD,
             cancellable = true,
             method = "sendImmediately",
-            at = @At(value = "FIELD", target = "Lnet/minecraft/network/ClientConnection;packetsSentCounter:I", opcode = Opcodes.GETFIELD))
+            at = @At(value = "FIELD", target = "Lnet/minecraft/network/ClientConnection;packetsSentCounter:I", opcode = Opcodes.GETFIELD, shift = At.Shift.AFTER))
     private void sendImmediately$rewrite(Packet<?> packet, @Nullable GenericFutureListener<? extends Future<? super Void>> callback, CallbackInfo info, NetworkState packetState, NetworkState protocolState) {
         boolean newState = packetState != protocolState;
 

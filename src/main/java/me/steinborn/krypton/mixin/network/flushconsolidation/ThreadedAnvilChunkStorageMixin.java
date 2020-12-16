450833f5e9056419ce2c78a484f3b880d43d1605
Andrew Steinborn <git@steinborn.me>
2020-12-16 15:53:59 -0500

    Implement entity tracker flush consolidation
    Based on the Tuinity patch: https://github.com/Spottedleaf/Tuinity/blob/master/patches/server/0018-Consolidate-flush-calls-for-entity-tracker-packets.patch

--- src/main/java/me/steinborn/krypton/mixin/network/flushconsolidation/ThreadedAnvilChunkStorageMixin.java
+++ src/main/java/me/steinborn/krypton/mixin/network/flushconsolidation/ThreadedAnvilChunkStorageMixin.java
@@ -3,28 +3,47 @@ package me.steinborn.krypton.mixin.network.flushconsolidation;
 import me.steinborn.krypton.mod.network.ConfigurableAutoFlush;
 import net.minecraft.network.Packet;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.server.world.ThreadedAnvilChunkStorage;
 import net.minecraft.world.chunk.WorldChunk;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ThreadedAnvilChunkStorage.class)
 public class ThreadedAnvilChunkStorageMixin {
+    @Shadow @Final private ServerWorld world;
+
     @Inject(method = "sendChunkDataPackets", at = @At("HEAD"))
     public void prepareToSendChunkDataPackets(ServerPlayerEntity player, Packet<?>[] packets, WorldChunk chunk, CallbackInfo info) {
-        // Guard against fake players
-        if (player.getClass() == ServerPlayerEntity.class) {
-            ((ConfigurableAutoFlush) player.networkHandler.getConnection()).setShouldAutoFlush(false);
-        }
+        setAutoFlush(player, false);
     }
 
     @Inject(method = "sendChunkDataPackets", at = @At("RETURN"))
     public void finishSendChunkDataPackets(ServerPlayerEntity player, Packet<?>[] packets, WorldChunk chunk, CallbackInfo info) {
-        // Guard against fake players
+        setAutoFlush(player, true);
+    }
+
+    @Inject(method = "tickPlayerMovement", at = @At("HEAD"))
+    public void disableAutoFlushForEntityTracking(CallbackInfo info) {
+        for (ServerPlayerEntity player : world.getPlayers()) {
+            setAutoFlush(player, false);
+        }
+    }
+
+    @Inject(method = "tickPlayerMovement", at = @At("RETURN"))
+    public void enableAutoFlushForEntityTracking(CallbackInfo info) {
+        for (ServerPlayerEntity player : world.getPlayers()) {
+            setAutoFlush(player, true);
+        }
+    }
+
+    private static void setAutoFlush(ServerPlayerEntity player, boolean val) {
         if (player.getClass() == ServerPlayerEntity.class) {
-            ((ConfigurableAutoFlush) player.networkHandler.getConnection()).setShouldAutoFlush(true);
+            ((ConfigurableAutoFlush) player.networkHandler.getConnection()).setShouldAutoFlush(val);
         }
     }
 }

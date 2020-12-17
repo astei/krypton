13c79b0653408cfb50856667335391b6a13b4efa
Andrew Steinborn <git@steinborn.me>
2020-12-17 12:02:12 -0500

    Optimize PacketByteBuf#writeString

---
+++ src/main/java/me/steinborn/krypton/mixin/network/shared/misc/PacketByteBufMixin.java
@@ -0,0 +1,39 @@
+package me.steinborn.krypton.mixin.network.shared.misc;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.handler.codec.EncoderException;
+import net.minecraft.network.PacketByteBuf;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+@Mixin(PacketByteBuf.class)
+public abstract class PacketByteBufMixin extends PacketByteBuf {
+    public PacketByteBufMixin(ByteBuf byteBuf) {
+        super(byteBuf);
+    }
+
+    @Shadow public abstract int writeCharSequence(CharSequence charSequence, Charset charset);
+
+    /**
+     * @reason Use {@link ByteBuf#writeCharSequence(CharSequence, Charset)} instead for improved performance along with
+     *         computing the byte size ahead of time with {@link ByteBufUtil#utf8Bytes(CharSequence)}.
+     */
+    @Overwrite
+    public PacketByteBuf writeString(String string, int i) {
+        int utf8Bytes = ByteBufUtil.utf8Bytes(string);
+        if (utf8Bytes > i) {
+            throw new EncoderException("String too big (was " + utf8Bytes + " bytes encoded, max " + i + ")");
+        } else {
+            this.writeVarInt(utf8Bytes);
+            this.writeCharSequence(string, StandardCharsets.UTF_8);
+            return this;
+        }
+    }
+
+    @Shadow public abstract PacketByteBuf writeVarInt(int i);
+}

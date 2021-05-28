a5d0313372e5fad190df03367e4564eb77cbc6f0
Andrew Steinborn <git@steinborn.me>
2021-05-27 23:04:03 -0400

    Optimize VarInt writing
    While all cases of VarInt writing are optimized, we pay special attention to the 1/2-byte VarInt cases as they are the most common. This has been corroborated by determining the counts of the byte sizes of each VarInt, and it's dramatically skewed towards smaller VarInts. It pays!

--- src/main/java/me/steinborn/krypton/mixin/network/shared/misc/PacketByteBufMixin.java
+++ src/main/java/me/steinborn/krypton/mixin/network/shared/misc/PacketByteBufMixin.java
@@ -3,14 +3,21 @@ package me.steinborn.krypton.mixin.network.shared.misc;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufUtil;
 import io.netty.handler.codec.EncoderException;
+import me.steinborn.krypton.mod.shared.KryptonSharedInitializer;
+import me.steinborn.krypton.mod.shared.network.util.VarIntUtil;
 import net.minecraft.network.PacketByteBuf;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.atomic.LongAdder;
 
 @Mixin(PacketByteBuf.class)
 public abstract class PacketByteBufMixin extends ByteBuf {
@@ -18,6 +25,15 @@ public abstract class PacketByteBufMixin extends ByteBuf {
     @Shadow @Final private ByteBuf parent;
 
     @Shadow public abstract int writeCharSequence(CharSequence charSequence, Charset charset);
+
+    /**
+     * @author Andrew
+     * @reason Use optimized VarInt byte size lookup table
+     */
+    @Overwrite
+    public static int getVarIntLength(int value) {
+        return VarIntUtil.getVarIntLength(value);
+    }
 
     /**
      * @author Andrew
@@ -36,5 +52,44 @@ public abstract class PacketByteBufMixin extends ByteBuf {
         }
     }
 
-    @Shadow public abstract PacketByteBuf writeVarInt(int i);
+    /**
+     * @author Andrew
+     * @reason optimized VarInt writing
+     */
+    @Overwrite
+    public PacketByteBuf writeVarInt(int value) {
+        // Peel the one and two byte count cases explicitly as they are the most common VarInt sizes
+        // that the proxy will write, to improve inlining.
+        if ((value & (0xFFFFFFFF << 7)) == 0) {
+            parent.writeByte(value);
+        } else if ((value & (0xFFFFFFFF << 14)) == 0) {
+            int w = (value & 0x7F | 0x80) << 8 | (value >>> 7);
+            parent.writeShort(w);
+        } else {
+            writeVarIntFull(parent, value);
+        }
+        return new PacketByteBuf(parent);
+    }
+
+    private static void writeVarIntFull(ByteBuf buf, int value) {
+        // See https://steinborn.me/posts/performance/how-fast-can-you-write-a-varint/
+        if ((value & (0xFFFFFFFF << 7)) == 0) {
+            buf.writeByte(value);
+        } else if ((value & (0xFFFFFFFF << 14)) == 0) {
+            int w = (value & 0x7F | 0x80) << 8 | (value >>> 7);
+            buf.writeShort(w);
+        } else if ((value & (0xFFFFFFFF << 21)) == 0) {
+            int w = (value & 0x7F | 0x80) << 16 | ((value >>> 7) & 0x7F | 0x80) << 8 | (value >>> 14);
+            buf.writeMedium(w);
+        } else if ((value & (0xFFFFFFFF << 28)) == 0) {
+            int w = (value & 0x7F | 0x80) << 24 | (((value >>> 7) & 0x7F | 0x80) << 16)
+                    | ((value >>> 14) & 0x7F | 0x80) << 8 | (value >>> 21);
+            buf.writeInt(w);
+        } else {
+            int w = (value & 0x7F | 0x80) << 24 | ((value >>> 7) & 0x7F | 0x80) << 16
+                    | ((value >>> 14) & 0x7F | 0x80) << 8 | ((value >>> 21) & 0x7F | 0x80);
+            buf.writeInt(w);
+            buf.writeByte(value >>> 28);
+        }
+    }
 }

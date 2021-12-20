b251fa07376ab4fb8333171c85ebcfa9e50779f1
Andrew Steinborn <git@steinborn.me>
2021-12-19 19:51:09 -0500

    Fix incompatibility with Immersive Portals

---
+++ src/main/java/me/steinborn/krypton/mod/shared/KryptonMixinPlugin.java
@@ -0,0 +1,49 @@
+package me.steinborn.krypton.mod.shared;
+
+import net.fabricmc.loader.api.FabricLoader;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+import java.util.List;
+import java.util.Set;
+
+public class KryptonMixinPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if (mixinClassName.contains("avoidwork.ThreadedAnvilChunkStorageMixin")) {
+            // This mixin is incompatible with Immersive Portals.
+            return !FabricLoader.getInstance().isModLoaded("imm_ptl_core");
+        }
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+}

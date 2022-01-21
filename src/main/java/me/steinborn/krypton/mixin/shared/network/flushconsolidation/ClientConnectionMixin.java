ed810e63bd3ed6b3a04c938d4ea833bcc0862e29
Emma <emmaffle@modrinth.com>
2022-01-21 16:46:49 -0500

    Fix crash on 22w03a by removing unnecessary shadowed logger (#60)

--- src/main/java/me/steinborn/krypton/mixin/shared/network/flushconsolidation/ClientConnectionMixin.java
+++ src/main/java/me/steinborn/krypton/mixin/shared/network/flushconsolidation/ClientConnectionMixin.java
@@ -7,10 +7,8 @@ import me.steinborn.krypton.mod.shared.network.ConfigurableAutoFlush;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.NetworkState;
 import net.minecraft.network.Packet;
-import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
 import org.objectweb.asm.Opcodes;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -30,8 +28,6 @@ public abstract class ClientConnectionMixin implements ConfigurableAutoFlush {
     private AtomicBoolean autoFlush;
 
     @Shadow public abstract void setState(NetworkState state);
-
-    @Shadow @Final private static Logger LOGGER;
 
     @Inject(method = "<init>", at = @At("RETURN"))
     private void initAddedFields(CallbackInfo ci) {
